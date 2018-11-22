7eb123d3686127bd485bedd92e75cf71f8e6ca07
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-11-22 11:11:18 -0500

    init

---
+++ Assets/Script/movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class movement : MonoBehaviour {
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+            }
+        }
+    }
+}
