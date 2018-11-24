d411b2cae56e3108928f8c6b84a24ecf9061f2db
Zachary Lockhart <zacharyjordan82@gmail.com>
2018-11-23 19:17:37 -0500

    adding missle

---
+++ Assets/Script/enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+    public int Health;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("enter");
+        if (col.gameObject.name == "missle")
+        {
+            Destroy(col.gameObject);
+            Health -= 10;
+        }
+    }
+}
